81e0d48809ef1affb4714398369263576f014d25
shibalike <116474528+shibalike@users.noreply.github.com>
2022-11-20 10:19:52 +0900

    0

--- Assets/bgGenerator.cs
+++ Assets/bgGenerator.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class bgGenerator : MonoBehaviour
 {
-    public GameObject bgPrefab;
+    public GameObject wallPrefab;
+    public GameObject[] bgPrefab;
     public GameObject vertical;
     public GameObject landscape;
-    public GameObject player; 
     public int N;
     int[][] map;
     void Start ()
     {
-        map = new int[100][]; //맵 9 * 9 크기의 2차원 배열 생성
+        map = new int[100][]; //맵 100 * 100 크기의 2차원 배열 생성
         for(int i = 0; i < 100; i++)
         {
             map[i] = new int[100];
@@ -21,7 +21,7 @@ public class bgGenerator : MonoBehaviour
         }
         //0은 빈 곳, 1은 방, 2는 길
         map[50][50] = 1;
-        GameObject G = Instantiate(bgPrefab);
+        GameObject G = Instantiate(wallPrefab);
         G.transform.position = new Vector3(2150, 2150, 0);
         mapArrange();
         
@@ -31,10 +31,12 @@ public class bgGenerator : MonoBehaviour
     {
         int y = 50, x = 50;
         int roadY, roadX;
-        int rotater;
+        int roadRotater;
+        int mapRand;
         for (int i = 0; i < N; i++)
         {
             int dir = Random.Range(0, 4);
+            mapRand = Random.Range(0, 6);
             if (dir == 0)
             {
                 map[y - 1][x] = 2;
@@ -42,7 +44,7 @@ public class bgGenerator : MonoBehaviour
                 roadY = y - 1;
                 roadX = x;
                 y -= 2;
-                rotater = 0;
+                roadRotater = 0;
             }
             else if (dir == 1)
             {
@@ -51,7 +53,7 @@ public class bgGenerator : MonoBehaviour
                 roadY = y;
                 roadX = x - 1;
                 x -= 2;
-                rotater = 1;
+                roadRotater = 1;
             }
             else if (dir == 2)
             {
@@ -60,7 +62,7 @@ public class bgGenerator : MonoBehaviour
                 roadY = y + 1;
                 roadX = x;
                 y += 2;
-                rotater = 0;
+                roadRotater = 0;
             }
             else
             {
@@ -69,15 +71,17 @@ public class bgGenerator : MonoBehaviour
                 roadY = y;
                 roadX = x + 1;
                 x += 2;
-                rotater = 1;
+                roadRotater = 1;
             }
 
             if (map[x][y] == 0 && map[roadX][roadY] == 0)
             {
-                GameObject G = Instantiate(bgPrefab);
-                G.transform.position = new Vector3(x * 43, y * 43, 0);
-                
-                if (rotater == 0)
+                GameObject room = Instantiate(wallPrefab);
+                room.transform.position = new Vector3(x * 43, y * 43, 0);
+                GameObject BG = Instantiate(bgPrefab[mapRand]);
+                BG.transform.position = new Vector3(x * 43, y * 43, 0);
+
+                if (roadRotater == 0)
                 {
                     GameObject vert = Instantiate(vertical);
                     vert.transform.position = new Vector3(roadX * 43, roadY * 43, 0);
@@ -87,9 +91,33 @@ public class bgGenerator : MonoBehaviour
                     GameObject land = Instantiate(landscape);
                     land.transform.position = new Vector3(roadX * 43, roadY * 43, 0);
                 }
+                map[x][y] = 1;
+                map[roadX][roadY] = 2;
             }
             else
-                i -= 1;
+            {
+                i -= 1; 
+                if (dir == 0)
+                {
+                    roadY = y + 1;
+                    y += 2;
+                }
+                else if (dir == 1)
+                {
+                    roadX = x + 1;
+                    x += 2;
+                }
+                else if (dir == 2)
+                {
+                    roadY = y - 1;
+                    y -= 2;
+                }
+                else
+                {
+                    roadX = x - 1;
+                    x -= 2;
+                }
+            }
         }
     }
 }
